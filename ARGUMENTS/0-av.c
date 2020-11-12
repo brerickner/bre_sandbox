e53c10010c489702da5dc443c54b850e1a652f03
brerickner <breannarickner@gmail.com>
2020-11-12 18:29:34 +0000

    added a print 1 function to print the space and newline

--- ARGUMENTS/0-av.c
+++ ARGUMENTS/0-av.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+print1(char c)
+{
+	return (write(1, &c, 1));
+}
 int main (int ac, char **av)
 {
 	(void)ac;
@@ -9,6 +13,8 @@ int main (int ac, char **av)
 	for (index = 0; av[index]; index++)
 	{
 		write(STDOUT_FILENO, av[index], strlen(av[index]));
+		print1(' ');
 	}
+	print1('\n');
 	return (0);
 }
