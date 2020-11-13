b9d7d542a5997ced44561cc22bfd0ae490a2e512
brerickner <breannarickner@gmail.com>
2020-11-13 05:07:17 +0000

    added comments

--- ARGUMENTS/prompt.c
+++ ARGUMENTS/prompt.c
@@ -9,20 +9,21 @@ int print_string(char *str)
 int main (int ac, char **av)
 {
 	char prompt[] = "$ ";
-	char *buffer = " ";
-	size_t buffSize = 0;
+	char *buffer = " "; /*must be initialized or memory leaks present*/
+	size_t buffSize = 0; /*initialized for memory leak purposes */
 	(void)ac;
 
-	av[0] = prompt;
-	print_string(prompt);
+	av[0] = prompt; /*assigns prompt to 0 index */
+	print_string(prompt); /*prints prompt $ */
+
 	buffer = malloc(strlen(buffer));
 	if (buffer == NULL)
 	{
 		perror("Unable to allocate memory");
 		exit(1);
 	}
-	free (buffer);
-      	getline(&buffer, &buffSize, stdin);
+	free (buffer); /*freeing the byte used to initialize */
+      	getline(&buffer, &buffSize, stdin); /*reallocs to what it needs?*/
 	print_string(buffer);
 	free(buffer);
 	return (0);
